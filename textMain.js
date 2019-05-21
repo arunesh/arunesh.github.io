86e18662e11ded9efa67b383d256366714750e8e
Arunesh Mishra <aruneshm@gmail.com>
2019-05-20 19:08:31 -0700

    moar js

---
+++ textMain.js
@@ -0,0 +1,9 @@
+
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  return array[Math.floor(Math.random()*array.length)];
+}
+
